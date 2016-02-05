ce798f595a6c6cb107dc2d84fe6b2bc4f5125d61
fuzulejumepo <fuzulejumepo@sofort-mail.de>
2016-02-05 15:23:25 +0100

    code improved

--- LegoRobot4/src/main/Main.java
+++ LegoRobot4/src/main/Main.java
@@ -12,21 +12,9 @@ public class Main {
 	public static void main(String[] args) {
 		final Robot robot = new Robot();
 		
-//		float[] sample = { 1.0f };
-//
-//		for (int i = 0; i < 6; ++i) {
-//			robot.gyroSensor.getAngleMode().fetchSample(sample, 0);
-//			robot.ev3.getTextLCD().drawString("" + sample[0], 2, 2);
-//			robot.ev3.getKeys().waitForAnyPress();
-//		}
-		
 		KeyListener listener = new KeyListener() {
-			
 			@Override
-			public void keyReleased(Key k) {
-				// TODO Auto-generated method stub		
-			}
-			
+			public void keyReleased(Key k) {}
 			@Override
 			public void keyPressed(Key k) {
 				robot.bumperRightSensor.close();
@@ -37,18 +25,10 @@ public class Main {
 			}
 		};
 		Button.ENTER.addKeyListener(listener);
+
 		
-		// robot.ev3.getTextLCD().drawString("FollowLine", 2, 1);
-		//
-		// Strategy currentStrategy = new FollowLineStrategy(robot);
-		
-		
-		robot.ev3.getTextLCD().drawString("LabyrinthStrategy", 2, 1);
 		Strategy currentStrategy = new LabyrinthStrategy(robot);
 		currentStrategy.execute();
-
-		//Strategy currentStrategy = new SuspensionBridgeStrategy(robot);
-		//currentStrategy.execute();
 		
 	}
 
