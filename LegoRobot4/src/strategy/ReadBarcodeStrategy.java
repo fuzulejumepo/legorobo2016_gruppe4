aa581c0eef2efa382bb49d65259b0e71c9e4376c
xiaoqi0529 <jieqi0529@gmail.com>
2016-02-08 15:04:47 +0100

    Barcode Reading.

--- LegoRobot4/src/strategy/ReadBarcodeStrategy.java
+++ LegoRobot4/src/strategy/ReadBarcodeStrategy.java
@@ -4,14 +4,14 @@ import lejos.hardware.Sound;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3GyroSensor;
 import lejos.robotics.RegulatedMotor;
-import lejos.utility.Delay;
+//import lejos.utility.Delay;
 import main.Robot;
 
 public class ReadBarcodeStrategy extends Strategy {
 	public static final float lineThreshold = 0.2f;
-	public static final int sensorArmSearchOffset = 35;
 	public static final int wheelMotorSpeed = 270;
 	public static final int wheelMotorSpeedCorrection = 40;
+	public static final int tachoCountMax = 170;
 	
 	protected RegulatedMotor armMotor;
 	protected RegulatedMotor leftWheelMotor;
@@ -33,68 +33,73 @@ public class ReadBarcodeStrategy extends Strategy {
 	@Override
 	public void execute() {
 		leftWheelMotor.synchronizeWith(new RegulatedMotor[] {rightWheelMotor});
-		leftWheelMotor.setSpeed(wheelMotorSpeed);
-		rightWheelMotor.setSpeed(wheelMotorSpeed);
 		
 		robot.colorSensor.setCurrentMode(robot.colorSensor.getRedMode().getName());
 		robot.calibrateArm();
 		robot.centerArm();
 		
-		armMotor.rotate(-sensorArmSearchOffset);
-		
-		float[] sample = { 0.0f };
+		float[] sample1 = { 0.0f };
+		float[] sample2 = { 0.0f };
+		float[] sample3 = { 0.0f };
 		int counter = 0;
 		int sign = 1;
-		boolean isBarcode = true;
-		
-		armMotor.rotate(2 * sensorArmSearchOffset, true);
-		while (armMotor.isMoving()) {
-			armSensor.getRedMode().fetchSample(sample, 0);
-			if (sample[0] <= lineThreshold) {
-				isBarcode = false;
-				break;
-			}
-		}
-		armMotor.rotate(-sensorArmSearchOffset);
-		
-		if (!isBarcode) return;
 		
 		Sound.beepSequenceUp();
 		
 		gyroSensor.reset();
 		gyroSensor.getAngleMode().fetchSample(startDirection, 0);
 		
+		
+		leftWheelMotor.synchronizeWith(new RegulatedMotor[] {rightWheelMotor});
+
+		leftWheelMotor.startSynchronization();
 		leftWheelMotor.forward();
 		rightWheelMotor.forward();
-			
-		while (isBarcode) {
-			if (sign > 0) {
-				robot.ev3.getLED().setPattern(1);
-			}
-			else {
-				robot.ev3.getLED().setPattern(2);
-			}
-			
-			correctDirection();
-			isBarcode = false;
-			for (int i = 0; i < 25; i++) {
-				armSensor.getRedMode().fetchSample(sample, 0);
-				System.out.println(sample[0]);
-				if (sign * sample[0] > sign * lineThreshold) {
-					isBarcode = true;
-				}
-			}
+		leftWheelMotor.endSynchronization();
 
-			//Delay.msDelay(50);
-			sign = -sign;
+		int meanTacho = 0;	
+		
+		armSensor.getRedMode().fetchSample(sample1, 0);
+		armSensor.getRedMode().fetchSample(sample2, 0);
+		armSensor.getRedMode().fetchSample(sample3, 0);
+		System.out.println("	first color : "+sample1[0]+","+sample2[0]+","+sample3[0]);	
+		
+		if (sample1[0]>0.2 ||sample2[0]>0.2 ||sample3[0]>0.2 )
+			sign = 1;
+		else
+			sign = -1;
+			
+		while ( meanTacho < tachoCountMax){
+			
 			counter += 1;
+			leftWheelMotor.resetTachoCount();
+			rightWheelMotor.resetTachoCount();
+			
+			if (sign == 1)
+				System.out.println("	white");
+			else
+				System.out.println("	black");
+			
+			while ((sign*sample1[0] > sign*0.2 || sign*sample2[0] > sign*0.2 
+					|| sign*sample3[0] > sign*0.2 ) 
+					&& leftWheelMotor.getTachoCount() < tachoCountMax) {		
+				armSensor.getRedMode().fetchSample(sample1, 0);
+				armSensor.getRedMode().fetchSample(sample2, 0);
+				armSensor.getRedMode().fetchSample(sample3, 0);
+				System.out.println("	color : "+sample1[0]+","+sample2[0]+","+sample3[0]);
+			}
+						
+			sign = -sign;
+			meanTacho =(leftWheelMotor.getTachoCount()+rightWheelMotor.getTachoCount())/2;	
+
+			System.out.println("		mean: "+meanTacho);
 		}
 		
 		leftWheelMotor.stop();
 		rightWheelMotor.stop();
-		System.out.println("Barcode: " + counter);
+		System.out.println("	Barcode: " + counter);
 	}
-	
+/*	
 	private void correctDirection() {
 		float[] sample = { 0.0f };
 		int speedOffset;
@@ -107,5 +112,5 @@ public class ReadBarcodeStrategy extends Strategy {
 		leftWheelMotor.setSpeed(wheelMotorSpeed + speedOffset);
 		rightWheelMotor.setSpeed(wheelMotorSpeed - speedOffset);
 	}
-
+*/
 }
