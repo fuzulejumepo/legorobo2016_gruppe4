23753b85ad3b70b0962fdc8fc88183dee9c05d01
Laura <ubdch@student.kit.edu>
2016-02-09 11:49:52 +0100

    better Labyrinth mode

--- LegoRobot4/src/strategy/LabyrinthStrategy.java
+++ LegoRobot4/src/strategy/LabyrinthStrategy.java
@@ -1,10 +1,12 @@
 package strategy;
 
 import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.EV3GyroSensor;
 import lejos.hardware.sensor.EV3TouchSensor;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.robotics.RegulatedMotor;
 import lejos.robotics.SampleProvider;
+import lejos.robotics.navigation.DifferentialPilot;
 import main.Constants;
 import main.Robot;
 import main.Status;
@@ -16,12 +18,20 @@ public class LabyrinthStrategy extends Strategy {
 	protected EV3TouchSensor touchSensorRight;
 	protected EV3ColorSensor colorSensor;
 	protected EV3UltrasonicSensor ultraSensor;
+	protected EV3GyroSensor gyroSensor;
 	protected SampleProvider ultra;
+	private DifferentialPilot pilot;
+	
 	static int speed = 520;
-	static int factor = 1150;
+	float maxSpeed;
+	static int maxFactor;
+	static int factor = 1200; // 1150
+	double distance;
 
 	public LabyrinthStrategy(Robot robot) {
 		super(robot);
+//		this.pilot = robot.
+		this.gyroSensor = robot.gyroSensor;
 		this.armMotor = robot.sensorArmMotor;
 		this.leftWheelMotor = robot.leftWheelMotor;
 		this.rightWheelMotor = robot.rightWheelMotor;
@@ -34,7 +44,8 @@ public class LabyrinthStrategy extends Strategy {
 	@Override
 	public void execute() {
 		robot.ev3.getTextLCD().drawString("LabyrinthStrategy", 2, 2);
-		
+		maxSpeed= Math.min(leftWheelMotor.getMaxSpeed(), rightWheelMotor.getMaxSpeed());
+		float[] gyroSample = {0.0f};
 		float[] colorSamples = { 0.0f };
 		float[] touchedRight = new float[touchSensorRight.sampleSize()];
 		float[] distances = new float[ultraSensor.sampleSize()];
@@ -47,20 +58,46 @@ public class LabyrinthStrategy extends Strategy {
 			while (touchedRight[0] != 1.0 && colorSamples[0] < Constants.lineThreshold) {
 				touchSensorRight.fetchSample(touchedRight, 0);
 				ultra.fetchSample(distances, 0);
-				double distance = distances[0] - 0.07;
+				distance = distances[0] - 0.07;
+				if (distance < 0.02 && touchedRight[0]!= 1.0){
+					while(distance<0.02 && touchedRight[0]!= 1.0){
+					moveCorrected(maxSpeed,maxFactor,distance);
+					ultra.fetchSample(distances, 0);
+					distance = distances[0] - 0.07;
+					touchSensorRight.fetchSample(touchedRight, 0);
+					}
+					while(distance>0.4){
+						leftWheelMotor.backward();
+						rightWheelMotor.backward();
+						ultra.fetchSample(distances, 0);
+						distance = distances[0] - 0.07;
+						}
+				}
+				
 				// lcd.drawString(" " + distance, 3, 3);
-				leftWheelMotor.setSpeed((int) (speed + (factor * distance)));
-				rightWheelMotor.setSpeed((int) (speed + - (factor * distance)));
-				leftWheelMotor.forward();
-				rightWheelMotor.forward();
+				moveCorrected(speed, factor, distance);
 				colorSensor.getRedMode().fetchSample(colorSamples, 0);
 			}
-			leftWheelMotor.rotate(-350, false);
+			gyroSensor.reset();
+			gyroSensor.getAngleMode().fetchSample(gyroSample, 0);
+			while (gyroSample[0] < 45.0f&& colorSamples[0] < Constants.lineThreshold){
+				leftWheelMotor.rotate(-350, false);
+				gyroSensor.getAngleMode().fetchSample(gyroSample, 0);
+			}
+//			leftWheelMotor.rotate(-350, false);
 			touchSensorRight.fetchSample(touchedRight, 0);
+			
 		}
 		
 		leftWheelMotor.stop();
 		rightWheelMotor.stop();
 		robot.setStatus(Status.BARCODE_FIND);
+	}
+
+	private void moveCorrected(float speed, int factor, double distance ) {
+		leftWheelMotor.setSpeed((int) (speed + (factor * distance)));
+		rightWheelMotor.setSpeed((int) (speed + -(factor * distance)));
+		leftWheelMotor.forward();
+		rightWheelMotor.forward();
 	}
 }
