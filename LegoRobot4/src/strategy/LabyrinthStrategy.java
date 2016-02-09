d85aa51b43198c837f18e509131d97d11add1adf
Laura <ubdch@student.kit.edu>
2016-02-09 14:48:38 +0100

    Labyrinth, working mode

--- LegoRobot4/src/strategy/LabyrinthStrategy.java
+++ LegoRobot4/src/strategy/LabyrinthStrategy.java
@@ -22,11 +22,12 @@ public class LabyrinthStrategy extends Strategy {
 	protected SampleProvider ultra;
 	private DifferentialPilot pilot;
 	
-	static int speed = 520;
+	static int speed = 500;
+	static int factor = 900; // 1150
 	float maxSpeed;
-	static int maxFactor;
-	static int factor = 1200; // 1150
-	double distance;
+	static float maxFactor;
+
+	float distance;
 
 	public LabyrinthStrategy(Robot robot) {
 		super(robot);
@@ -44,33 +45,34 @@ public class LabyrinthStrategy extends Strategy {
 	@Override
 	public void execute() {
 		robot.ev3.getTextLCD().drawString("LabyrinthStrategy", 2, 2);
-		maxSpeed= Math.min(leftWheelMotor.getMaxSpeed(), rightWheelMotor.getMaxSpeed());
+		maxSpeed= Math.min(leftWheelMotor.getMaxSpeed(), rightWheelMotor.getMaxSpeed()) -100;
+		maxFactor = maxSpeed * 2;
 		float[] gyroSample = {0.0f};
 		float[] colorSamples = { 0.0f };
 		float[] touchedRight = new float[touchSensorRight.sampleSize()];
 		float[] distances = new float[ultraSensor.sampleSize()];
 		leftWheelMotor.forward();
 		rightWheelMotor.forward();
-		
+		colorSensor.setCurrentMode(robot.colorSensor.getRedMode().getName());
 		colorSensor.getRedMode().fetchSample(colorSamples, 0);
 		
 		while (colorSamples[0] < Constants.lineThreshold) {
 			while (touchedRight[0] != 1.0 && colorSamples[0] < Constants.lineThreshold) {
 				touchSensorRight.fetchSample(touchedRight, 0);
 				ultra.fetchSample(distances, 0);
-				distance = distances[0] - 0.07;
+				distance = distances[0] - 0.07f;
 				if (distance < 0.02 && touchedRight[0]!= 1.0){
 					while(distance<0.02 && touchedRight[0]!= 1.0){
 					moveCorrected(maxSpeed,maxFactor,distance);
 					ultra.fetchSample(distances, 0);
-					distance = distances[0] - 0.07;
+					distance = distances[0] - 0.07f;
 					touchSensorRight.fetchSample(touchedRight, 0);
 					}
 					while(distance>0.4){
 						leftWheelMotor.backward();
 						rightWheelMotor.backward();
 						ultra.fetchSample(distances, 0);
-						distance = distances[0] - 0.07;
+						distance = distances[0] - 0.07f;
 						}
 				}
 				
@@ -94,7 +96,7 @@ public class LabyrinthStrategy extends Strategy {
 		robot.setStatus(Status.BARCODE_FIND);
 	}
 
-	private void moveCorrected(float speed, int factor, double distance ) {
+	private void moveCorrected(float speed, float factor, float distance ) {
 		leftWheelMotor.setSpeed((int) (speed + (factor * distance)));
 		rightWheelMotor.setSpeed((int) (speed + -(factor * distance)));
 		leftWheelMotor.forward();
